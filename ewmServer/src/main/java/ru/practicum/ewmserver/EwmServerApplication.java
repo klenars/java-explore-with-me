c6c659a349d4f452eca2a3e5d0175d9337475202
klenars <klen_ars@mail.ru>
2022-10-11 17:36:20 +0300

    #1 Initial

---
+++ ewmServer/src/main/java/ru/practicum/ewmserver/EwmServerApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwmServerApplication.class, args);
+    }
+
+}
