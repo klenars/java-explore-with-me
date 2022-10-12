a2d8c420677ad26cc6cd83bb6f38a62533378f6d
klenars <klen_ars@mail.ru>
2022-10-12 09:56:36 +0300

    #3 ewmStat

---
+++ ewmStat/src/test/java/ru/practicum/ewmstat/EwmStatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewmstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwmStatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
